8c7c3ef9fc3dfd84cbbd15988933fdfe15c68f53
Dylan Goldsborough <dgoldsborough@flexport.com>
2023-12-09 10:59:33 +0100

    Day 9 part a

--- days/src/days_module/day_09.rs
+++ days/src/days_module/day_09.rs
@@ -1,6 +1,30 @@
 use crate::days_module::day::Day;
+use helpers::ints_from_string;
 
 pub struct Day09 {}
+
+fn extrapolate_delta(series: &Vec<isize>) -> isize {
+    // Base case.
+    if series.iter().all(|i| i == &0) {
+        return 0;
+    }
+
+    // Recurse.
+    let differences = series
+        .iter()
+        .zip(series.iter().skip(1))
+        .map(|(a, b)| b - a)
+        .collect::<Vec<isize>>();
+    let delta = extrapolate_delta(&differences);
+    let last_difference = differences.last().unwrap();
+    return last_difference + delta
+}
+
+fn extrapolate(series: &Vec<isize>) -> isize {
+    let last_number = *series.last().unwrap();
+    let delta = extrapolate_delta(series);
+    last_number + delta
+}
 
 impl Day for Day09 {
     fn get_id(&self) -> String {
@@ -8,7 +32,12 @@ impl Day for Day09 {
     }
 
     fn part_a(&self, input: &String) -> String {
-        "".to_string()
+        input
+            .split("\n")
+            .map(ints_from_string)
+            .map(|s| extrapolate(&s))
+            .sum::<isize>()
+            .to_string()
     }
 
     fn part_b(&self, input: &String) -> String {
