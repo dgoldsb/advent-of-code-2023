dc5a796100b59d5f4e0849135328fe90ac26ace4
Dylan Goldsborough <dgoldsborough@flexport.com>
2023-12-09 10:26:25 +0100

    Boilerplate day 9

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -7,6 +7,7 @@ use days::days_module::day_05::Day05;
 use days::days_module::day_06::Day06;
 use days::days_module::day_07::Day07;
 use days::days_module::day_08::Day08;
+use days::days_module::day_09::Day09;
 use helpers::read_file;
 use std::time::Instant;
 
@@ -29,6 +30,7 @@ fn main() {
     days.push(Box::new(Day06 {}));
     days.push(Box::new(Day07 {}));
     days.push(Box::new(Day08 {}));
+    days.push(Box::new(Day09 {}));
 
     let start = Instant::now();
     println!(
