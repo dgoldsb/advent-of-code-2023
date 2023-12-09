8788cc0eacc141e7039e5bc7207de6109bf37db7
Dylan Goldsborough <dgoldsborough@flexport.com>
2023-12-09 11:05:48 +0100

    Day 9 part b

--- days/src/days_module/day_09.rs
+++ days/src/days_module/day_09.rs
@@ -16,14 +16,37 @@ fn extrapolate_delta(series: &Vec<isize>) -> isize {
         .map(|(a, b)| b - a)
         .collect::<Vec<isize>>();
     let delta = extrapolate_delta(&differences);
-    let last_difference = differences.last().unwrap();
-    return last_difference + delta
+    let last_difference = *differences.last().unwrap();
+    return last_difference + delta;
 }
 
 fn extrapolate(series: &Vec<isize>) -> isize {
     let last_number = *series.last().unwrap();
     let delta = extrapolate_delta(series);
     last_number + delta
+}
+
+fn extrapolate_delta_backwards(series: &Vec<isize>) -> isize {
+    // Base case.
+    if series.iter().all(|i| i == &0) {
+        return 0;
+    }
+
+    // Recurse.
+    let differences = series
+        .iter()
+        .zip(series.iter().skip(1))
+        .map(|(a, b)| b - a)
+        .collect::<Vec<isize>>();
+    let delta = extrapolate_delta_backwards(&differences);
+    let first_difference = *differences.first().unwrap();
+    return first_difference - delta;
+}
+
+fn extrapolate_backwards(series: &Vec<isize>) -> isize {
+    let first_number = *series.first().unwrap();
+    let delta = extrapolate_delta_backwards(series);
+    first_number - delta
 }
 
 impl Day for Day09 {
@@ -41,7 +64,12 @@ impl Day for Day09 {
     }
 
     fn part_b(&self, input: &String) -> String {
-        "".to_string()
+        input
+            .split("\n")
+            .map(ints_from_string)
+            .map(|s| extrapolate_backwards(&s))
+            .sum::<isize>()
+            .to_string()
     }
 }
 
