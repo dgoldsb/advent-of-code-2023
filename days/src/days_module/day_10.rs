5dcc670e98ebf5963d4324a75f7d996eb18405d2
Dylan Goldsborough <dgoldsborough@flexport.com>
2023-12-10 18:52:01 +0100

    Finish day 10, but very hacky, needs cleanup for sure

--- days/src/days_module/day_10.rs
+++ days/src/days_module/day_10.rs
@@ -5,7 +5,7 @@ use std::collections::{HashMap, HashSet};
 fn find_char_index<'a>(target: char, map: &'a HashMap<(isize, isize), char>) -> &'a (isize, isize) {
     for (key, val) in map.iter() {
         if val == &target {
-            return key
+            return key;
         }
     }
     panic!("Target not found!")
@@ -23,12 +23,24 @@ pub fn parse_char_map(input: &String) -> HashMap<(isize, isize), char> {
 
 fn get_neighbors(pipe: &char, index: &(isize, isize)) -> Vec<(isize, isize)> {
     match pipe {
-        '|' => {vec![(index.0 + 1, index.1), (index.0 - 1, index.1)]},
-        '-' => {vec![(index.0, index.1 + 1), (index.0, index.1 - 1)]},
-        'L' => {vec![(index.0 - 1, index.1), (index.0, index.1 + 1)]},
-        'J' => {vec![(index.0 - 1, index.1), (index.0, index.1 - 1)]},
-        '7' => {vec![(index.0 + 1, index.1), (index.0, index.1 - 1)]},
-        'F' => {vec![(index.0 + 1, index.1), (index.0, index.1 + 1)]},
+        '|' => {
+            vec![(index.0 + 1, index.1), (index.0 - 1, index.1)]
+        }
+        '-' => {
+            vec![(index.0, index.1 + 1), (index.0, index.1 - 1)]
+        }
+        'L' => {
+            vec![(index.0 - 1, index.1), (index.0, index.1 + 1)]
+        }
+        'J' => {
+            vec![(index.0 - 1, index.1), (index.0, index.1 - 1)]
+        }
+        '7' => {
+            vec![(index.0 + 1, index.1), (index.0, index.1 - 1)]
+        }
+        'F' => {
+            vec![(index.0 + 1, index.1), (index.0, index.1 + 1)]
+        }
         _ => panic!("Invalid pipe piece!"),
     }
 }
@@ -61,7 +73,7 @@ impl Day for Day10 {
             }
 
             if updated == false {
-                break
+                break;
             }
         }
 
@@ -69,7 +81,83 @@ impl Day for Day10 {
     }
 
     fn part_b(&self, input: &String) -> String {
-        "".to_string()
+        // TODO: Part A, factor out.
+        let map = parse_char_map(input);
+        let mut visited = HashSet::new();
+        let mut index = find_char_index('S', &map).clone();
+        let mut char_ = '7';
+
+        loop {
+            visited.insert(index.clone());
+
+            let mut updated = false;
+            for neighbor in get_neighbors(&char_, &index) {
+                if !visited.contains(&neighbor) {
+                    index = neighbor;
+                    char_ = *map.get(&index).unwrap();
+                    updated = true;
+                }
+            }
+
+            if updated == false {
+                break;
+            }
+        }
+
+        // TODO: Now for part B.
+        let mut inner_squares = 0;
+        for (x, line) in input.split("\n").enumerate() {
+            let mut pipes = 0;
+            let mut els = 0;
+            let mut sevens = 0;
+            let mut efs = 0;
+            let mut jays = 0;
+
+            let mut inside = false;
+            let mut last_character: char = '.';
+
+            for (y, input_char) in line.chars().enumerate() {
+                if visited.contains(&(x as isize, y as isize)) {
+                    // We need to track if we are inside.
+                    // TODO: This needs to be cleaned up... A lot...
+                    inside = match input_char {
+                        '|' => !inside,
+                        'L' => {
+                            last_character = 'L';
+                            inside
+                        }
+                        '7' => {
+                            if last_character == 'L' {
+                                !inside
+                            } else if last_character == 'F' {
+                                inside
+                            } else {
+                                panic!("What!")
+                            }
+                        }
+                        'F' => {
+                            last_character = 'F';
+                            inside
+                        }
+                        'J' => {
+                            if last_character == 'F' {
+                                !inside
+                            } else if last_character == 'L' {
+                                inside
+                            } else {
+                                panic!("What!")
+                            }
+                        }
+                        _ => inside,
+                    }
+                } else {
+                    if inside {
+                        inner_squares += 1;
+                    }
+                }
+            }
+        }
+        inner_squares.to_string()
     }
 }
 
