2341b81a93deebf211cfc06b64834c197b7c0a5e
Dylan Goldsborough <dgoldsborough@flexport.com>
2023-12-12 13:41:20 +0100

    Day 12 part a

--- days/src/days_module/day_12.rs
+++ days/src/days_module/day_12.rs
@@ -1,5 +1,103 @@
 use crate::days_module::day::Day;
 
+fn calculate_checksum(configuration: &Vec<char>) -> Vec<usize> {
+    let mut streak = 0;
+    let mut result = Vec::new();
+
+    for char_ in configuration {
+        match *char_ {
+            '#' => streak += 1,
+            '.' => {
+                if streak > 0 {
+                    result.push(streak.clone());
+                    streak = 0;
+                }
+            }
+            // Consider `?` in the least restraining way.
+            '?' => {
+                if streak > 0 {
+                    result.push(streak.clone());
+                    streak = 0;
+                }
+            }
+            _ => panic!("Unknown character"),
+        }
+    }
+    if streak > 0 {
+        result.push(streak.clone());
+    }
+
+    result
+}
+
+fn verify_checksum(checksum: &Vec<usize>, reference: &Vec<usize>) -> Result<(), bool> {
+    if checksum.len() > reference.len() {
+        return Err(true);
+    } else if checksum.len() < reference.len() {
+        return Err(true);
+    }
+
+    for (check, reference) in checksum.iter().zip(reference.iter()) {
+        if check < reference {
+            return Err(true);
+        } else if check > reference {
+            return Err(true);
+        }
+    }
+
+    Ok(())
+}
+
+fn count_configuration(
+    configuration: &Vec<char>,
+    reference: &Vec<usize>,
+    last_mutated_index: Option<usize>,
+) -> usize {
+    match verify_checksum(&calculate_checksum(configuration), reference) {
+        Ok(()) => return 1,
+        Err(false) => return 0,
+        Err(true) => {}
+    }
+
+    let next_mutated_index_result = find_unknown(configuration, last_mutated_index);
+
+    if next_mutated_index_result.is_none() {
+        return 0;
+    }
+
+    let next_mutated_index = next_mutated_index_result.unwrap();
+    let mut cloned_configuration = configuration.clone();
+    cloned_configuration[next_mutated_index] = '.';
+    let defective_count =
+        count_configuration(&cloned_configuration, reference, Some(next_mutated_index));
+    cloned_configuration[next_mutated_index] = '#';
+    let effective_count =
+        count_configuration(&cloned_configuration, reference, Some(next_mutated_index));
+
+    return effective_count + defective_count;
+}
+
+fn find_unknown(configuration: &Vec<char>, previous_index: Option<usize>) -> Option<usize> {
+    configuration
+        .iter()
+        .enumerate()
+        .filter(|(i, c)| **c == '?' && (previous_index.is_none() || (i > &previous_index.unwrap())))
+        .map(|(i, _)| i)
+        .next()
+}
+
+fn parse_line(line: &str) -> (Vec<char>, Vec<usize>) {
+    let mut split = line.split(" ");
+    let configuration = split.next().unwrap().chars().collect::<Vec<char>>();
+    let reference = split
+        .next()
+        .unwrap()
+        .split(",")
+        .map(|i| i.parse().unwrap())
+        .collect::<Vec<usize>>();
+    (configuration, reference)
+}
+
 pub struct Day12 {}
 
 impl Day for Day12 {
@@ -8,7 +106,12 @@ impl Day for Day12 {
     }
 
     fn part_a(&self, input: &String) -> String {
-        "0".to_string()
+        input
+            .split("\n")
+            .map(parse_line)
+            .map(|t| count_configuration(&t.0, &t.1, None))
+            .sum::<usize>()
+            .to_string()
     }
 
     fn part_b(&self, input: &String) -> String {
