228852e577d1e63ae8cce529975b7a8ced871d29
Dylan Goldsborough <dgoldsborough@flexport.com>
2023-12-24 13:05:32 +0100

    Day 19 part b

--- days/src/days_module/day_19.rs
+++ days/src/days_module/day_19.rs
@@ -1,6 +1,69 @@
 use crate::days_module::day::Day;
 use std::collections::{HashMap, VecDeque};
+use std::ops::Range;
 use std::str::FromStr;
+
+#[derive(Clone)]
+struct MachinePartSpace {
+    x: Range<usize>,
+    m: Range<usize>,
+    a: Range<usize>,
+    s: Range<usize>,
+}
+
+impl MachinePartSpace {
+    fn sum(&self) -> usize {
+        self.x.len() * self.m.len() * self.a.len() * self.s.len()
+    }
+
+    fn access(&self, char_: &char) -> Range<usize> {
+        match char_ {
+            'x' => self.x.clone(),
+            'm' => self.m.clone(),
+            'a' => self.a.clone(),
+            's' => self.s.clone(),
+            _ => panic!("Invalid property {}", char_),
+        }
+    }
+
+    fn copy_with(&self, char_: &char, value: Range<usize>) -> MachinePartSpace {
+        match char_ {
+            'x' => self.with_x(value),
+            'm' => self.with_m(value),
+            'a' => self.with_a(value),
+            's' => self.with_s(value),
+            _ => panic!("Invalid property {}", char_),
+        }
+    }
+
+    // Function to create a new instance with the 'x' attribute overridden
+    fn with_x(&self, x: Range<usize>) -> MachinePartSpace {
+        let mut cloned = self.clone();
+        cloned.x = x;
+        cloned
+    }
+
+    // Function to create a new instance with the 'm' attribute overridden
+    fn with_m(&self, m: Range<usize>) -> MachinePartSpace {
+        let mut cloned = self.clone();
+        cloned.m = m;
+        cloned
+    }
+
+    // Function to create a new instance with the 'a' attribute overridden
+    fn with_a(&self, a: Range<usize>) -> MachinePartSpace {
+        let mut cloned = self.clone();
+        cloned.a = a;
+        cloned
+    }
+
+    // Function to create a new instance with the 's' attribute overridden
+    fn with_s(&self, s: Range<usize>) -> MachinePartSpace {
+        let mut cloned = self.clone();
+        cloned.s = s;
+        cloned
+    }
+}
 
 struct MachinePart {
     x: usize,
@@ -58,6 +121,38 @@ impl Rule {
         match self.sign {
             '<' => value < &self.value,
             '>' => value > &self.value,
+            _ => panic!("Invalid operator {}", self.sign),
+        }
+    }
+
+    // First value is the matching range, second is the non-matching range.
+    fn test_range(&self, input_range: Range<usize>) -> (Range<usize>, Range<usize>) {
+        let mut split_value = self.value;
+
+        // Because ranges are exclusive at the end, adjust the returned ranged in case of `>`.
+        if self.sign == '>' {
+            split_value += 1;
+        }
+
+        let first_range;
+        let second_range;
+        if split_value < input_range.start {
+            // Split value is before the input range, return an empty range and the original range.
+            first_range = input_range.start..input_range.start;
+            second_range = input_range;
+        } else if split_value >= input_range.end {
+            // Split value is after or at the end of the input range, return the original range and
+            // an empty range.
+            first_range = input_range.clone();
+            second_range = input_range.end..input_range.end;
+        } else {
+            // Split value is within the input range, create two ranges accordingly.
+            first_range = input_range.start..split_value;
+            second_range = split_value..input_range.end;
+        }
+        match self.sign {
+            '<' => (first_range, second_range),
+            '>' => (second_range, first_range),
             _ => panic!("Invalid operator {}", self.sign),
         }
     }
@@ -119,6 +214,65 @@ impl WorkflowRule {
                 .unwrap()
                 .test_part(part, workflow_map),
         };
+    }
+
+    // Return the accepted spaces.
+    fn test_space(
+        &self,
+        space: MachinePartSpace,
+        workflow_map: &HashMap<String, WorkflowRule>,
+    ) -> (Vec<MachinePartSpace>, Vec<MachinePartSpace>) {
+        let mut remaining_spaces = vec![space];
+        let mut accepted_spaces = Vec::new();
+        let mut rejected_spaces = Vec::new();
+
+        for (property, rule) in &self.rules {
+            let mut new_remaining_spaces = Vec::new();
+
+            for remaining_space in remaining_spaces {
+                let (target_range, remain_range) =
+                    rule.test_range(remaining_space.access(property));
+
+                new_remaining_spaces.push(remaining_space.copy_with(property, remain_range));
+
+                match self.rule_result_map.get(&rule).unwrap() {
+                    Ok(true) => {
+                        accepted_spaces.push(remaining_space.copy_with(property, target_range))
+                    }
+                    Ok(false) => {
+                        rejected_spaces.push(remaining_space.copy_with(property, target_range))
+                    }
+                    Err(other_rule) => {
+                        let rule = workflow_map.get(other_rule).unwrap();
+
+                        let (accepted, rejected) = rule.test_space(
+                            remaining_space.copy_with(property, target_range),
+                            workflow_map,
+                        );
+                        accepted_spaces.extend(accepted.iter().map(|c| c.clone()));
+                        rejected_spaces.extend(rejected.iter().map(|c| c.clone()));
+                    }
+                };
+            }
+
+            remaining_spaces = new_remaining_spaces;
+        }
+
+        match &self.default {
+            Ok(true) => accepted_spaces.extend(remaining_spaces.iter().map(|c| c.clone())),
+            Ok(false) => rejected_spaces.extend(remaining_spaces.iter().map(|c| c.clone())),
+            Err(other_rule) => {
+                let rule = workflow_map.get(other_rule).unwrap();
+
+                for remaining_space in remaining_spaces {
+                    let (accepted, rejected) = rule.test_space(remaining_space, workflow_map);
+                    accepted_spaces.extend(accepted.iter().map(|c| c.clone()));
+                    rejected_spaces.extend(rejected.iter().map(|c| c.clone()));
+                }
+            }
+        };
+
+        return (accepted_spaces, rejected_spaces);
     }
 }
 
@@ -224,7 +378,21 @@ impl Day for Day19 {
     // We are not iterating over the possible parts, instead we take a space as input and return the
     // subspaces that are accepted. We then sum the sizes of the returned spaces.
     fn part_b(&self, input: &String) -> String {
-        "".to_string()
+        let (cache, _) = parse(input);
+
+        let input_space = MachinePartSpace {
+            x: 1..4001,
+            m: 1..4001,
+            a: 1..4001,
+            s: 1..4001,
+        };
+        let in_workflow: &WorkflowRule = cache.get("in").unwrap();
+        let (accepted, _) = in_workflow.test_space(input_space, &cache);
+        accepted
+            .iter()
+            .map(|s| s.sum().clone())
+            .sum::<usize>()
+            .to_string()
     }
 }
 
